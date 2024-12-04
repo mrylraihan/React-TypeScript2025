9b1618879ec1b9eeedf8bdcc491b9ef587b5e970
mrylraihan <mr.ylraihan@gmail.com>
2024-12-03 19:24:47 -0800

    remove ZOD

--- ZODPractice/my-next-app/src/pages/api/create.js
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import { createTodo } from "@/supabase"
-
-export default async function handler(req, res) {
-    console.log(req.body)
-    const data = await createTodo(req.body)
-    return res.json(data)
-}
